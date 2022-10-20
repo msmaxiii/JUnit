f072657d19d5f131d7ac068c1166e0d1832bb0c7
msmaxiii <maxwellrg@gmail.com>
2022-10-20 16:00:52 -0500

    first commit

---
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -0,0 +1,19 @@
+package com.in28minutes.junit.helper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringHelperTest {
+
+
+    @Test
+    void truncateAInFirst2Positions(){
+
+    }
+    @Test
+    void areFirstAndLastTwoCharactersTheSame(){
+
+    }
+
+}
